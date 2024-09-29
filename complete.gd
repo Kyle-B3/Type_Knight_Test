ab9919ac707752f2566448bc0589bd1042303780
Kyle-B3 <kylwboma@vols.utk.edu>
2024-09-29 11:49:01 -0400

    Delete complete.gd

--- complete.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-#Quits the game
-func _on_quit_game_pressed():
-	get_tree().quit()
-
-#Returns player to the home screen
-func _on_return_button_pressed():
-	get_tree().change_scene_to_file("res://Start Menu/menu.tscn")
